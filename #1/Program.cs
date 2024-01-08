e2bb991dbf1906127deecf0e43b628d37e7eeec3
Vlad Brata <147712579+vladbrata@users.noreply.github.com>
2024-01-08 12:33:15 +0200

    Problem Solution

--- #1/Program.cs
+++ #1/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Calculati suma elementelor unui vector de n numere citite de la tastatura. Rezultatul se va afisa pe ecran.
+
+Console.WriteLine("Cate elemente are vectorul?");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] vector = new int[n];
+
+GetVectorElements(n, vector);
+
+Console.WriteLine("Suma elementelor din vector este: " + GetVectorSum(vector));
+
+static int GetVectorSum(int[] vector)
+{
+    int vectorSum = 0;
+
+    foreach (int element in vector) {
+        vectorSum += element;
+    }
+    return vectorSum;
+}
+
+static void GetVectorElements(int n, int[] vector)
+{
+    int i = 0;
+    
+    while (i < n) {
+        Console.WriteLine($"Care este elementul aflat pe pozitia {i} al vectorului?");
+        vector[i] = Convert.ToInt32(Console.ReadLine());
+        i++;
+    }
+}
