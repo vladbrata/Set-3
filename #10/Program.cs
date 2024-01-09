2925864cb4a7e5325bfd9dfd4812be00baff309e
Vlad Brata <147712579+vladbrata@users.noreply.github.com>
2024-01-09 13:14:47 +0200

    Enuntul problemei

--- #10/Program.cs
+++ #10/Program.cs
@@ -1,2 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Cautare binara. Se da un vector cu n elemente sortat in ordine crescatoare. Se cere sa se determine pozitia unui element dat k. Daca elementul nu se gaseste in vector rezultatul va fi -1.
