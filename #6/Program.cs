02cd94288ac597e3bff29f2d18ea229d61c438c6
Vlad Brata <147712579+vladbrata@users.noreply.github.com>
2024-01-09 11:54:32 +0200

    New Filr

--- #6/Program.cs
+++ #6/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Se da un vector cu n elemente si o pozitie din vector k. Se cere sa se stearga din vector elementul de pe pozitia k. Prin stergerea unui element, toate elementele din dreapta lui se muta cu o pozitie spre stanga.
+
+Console.Write("Cate numere are vectorul? n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] vector = GetVectorElements(n);
+PrintArr(vector);
+
+Console.Write("\nCe element doresti  sa elimini din vector? k: \n");
+int k = Convert.ToInt32(Console.ReadLine());
+
+if (k < n) {
+    vector = RemoveElement(vector, k);
+    Console.WriteLine("\nVectorul actualizat: ");
+    PrintArr(vector);
+}
+else {
+    Console.WriteLine($"Elementul {k} nu exista in vector.");
+}
+
+static int[] GetVectorElements(int n)
+{
+    int[] arr = new int[n];
+
+    int i = 1;
+    while (i < n) {
+        arr[i] = i;
+        i++;
+    }
+    return arr;
+}
+static void PrintArr(int[] arr) 
+{
+    foreach(int element in arr) {
+        Console.Write(element + " ");
+    }
+}
+static int[] RemoveElement(int[] vector, int k)
+{
+    int n = vector.Length;
+    int[] secondVector = new int[n - 1];
+
+    for (int i = 0; i < n - 1; i++) {
+        if (i < k) {
+            secondVector[i] = vector[i];
+        }
+        else if (k < n - 1) {
+            secondVector[i] = vector[i + 1];
+        }
+    }
+    
+    vector = new int[n - 1];
+    for (int i = 0; i < n - 1; i++) {
+        vector[i] = secondVector[i];
+    }
+    return vector;
+}
